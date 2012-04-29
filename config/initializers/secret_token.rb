beadb9801a9070f3165a449f0d59588e8327ac63
Gervel Giva <gervelgiva@gmail.com>
2012-04-29 11:48:24 -0400

    Working copy-Login and Signup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = 'f9116f587911c4fadc7914917a6a5cfdf97fd959feedc675802e7ee28a873d30f1fcef967c8fcb99b52f4cc1742ad07f274d596be18620b4f374ad398aeef215'
